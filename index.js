55d5ea03ef819082eea27b1ec4f2456917bce423
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-06 20:29:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 /**
  * Don't change these constants!
  */
-const DODGER = document.getElementById('dodger')
-const GAME = document.getElementById('game')
-const GAME_HEIGHT = 400
-const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
-const ROCKS = []
-const START = document.getElementById('start')
+const DODGER = document.getElementById('dodger');
+const GAME = document.getElementById('game');
+const GAME_HEIGHT = 400;
+const GAME_WIDTH = 400;
+const LEFT_ARROW = 37; // use e.which!
+const RIGHT_ARROW = 39; // use e.which!
+const ROCKS = [];
+const START = document.getElementById('start');
 
-var gameInterval = null
+var gameInterval = null;
 
 function checkCollision(rock) {
-  const top = positionToInteger(rock.style.top)
+  const top = positionToInteger(rock.style.top);
 
   if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
+    const dodgerLeftEdge = positionToInteger(DODGER.style.left);
     const dodgerRightEdge = dodgerLeftEdge + 40;
-    const rockLeftEdge = positionToInteger(rock.style.left)
+    const rockLeftEdge = positionToInteger(rock.style.left);
     const rockRightEdge = rockLeftEdge + 20;
 
     if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) || (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
-      return true
+      return true;
     }
   }
 }
 
 function createRock(x) {
-  const rock = document.createElement('div')
+  const rock = document.createElement('div');
 
-  rock.className = 'rock'
-  rock.style.left = `${x}px`
+  rock.className = 'rock';
+  rock.style.left = `${x}px`;
 
-  var top = rock.style.top = 0
+  var top = rock.style.top = 0;
 
   GAME.appendChild(rock);
 
@@ -45,72 +45,72 @@ function createRock(x) {
     }
     
     if (top < GAME_HEIGHT) {
-      window.requestAnimationFrame(moveRock)
+      window.requestAnimationFrame(moveRock);
     } else {
-      rock.remove()
+      rock.remove();
     }
   }
 
-  window.requestAnimationFrame(moveRock)
+  window.requestAnimationFrame(moveRock);
 
-  ROCKS.push(rock)
+  ROCKS.push(rock);
 
-  return rock
+  return rock;
 }
 
 
 function endGame() {
-  clearInterval(gameInterval)
+  clearInterval(gameInterval);
   
   ROCKS.forEach(function(rock) {
-    rock.remove()
-  })
+    rock.remove();
+  });
   
-  document.removeEventListener('keydown', moveDodger)
+  document.removeEventListener('keydown', moveDodger);
   
-  return alert("YOU LOSE!")
+  return alert("YOU LOSE!");
 }
 
 function moveDodger(e) {
-  const code = e.which
+  const code = e.which;
   
   if(code === LEFT_ARROW) {
-    moveDodgerLeft()
+    moveDodgerLeft();
   } else if (code === RIGHT_ARROW) {
-    moveDodgerRight()
+    moveDodgerRight();
   }
 }
 
 function moveDodgerLeft() {
   window.requestAnimationFrame(function(){
-    const left = positionToInteger(DODGER.style.left)
+    const left = positionToInteger(DODGER.style.left);
     
     if (left > 0){
       DODGER.style.left = `${left - 4}px`;
     }
-  })
+  });
 }
 
 function moveDodgerRight() {
   window.requestAnimationFrame(function(){
-    const left = positionToInteger(DODGER.style.left)
+    const left = positionToInteger(DODGER.style.left);
     
     if (left < 360) {
       DODGER.style.right = `${left + 4}px`;
     }
-  })
+  });
 }
 
 function positionToInteger(p) {
-  return parseInt(p.split('px')[0]) || 0
+  return parseInt(p.split('px')[0]) || 0;
 }
 
 function start() {
-  window.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)));
+  }, 1000);
 }
